25c3dbdfa18142d5b680a0a8fcb19a0160afd4cf
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-04-30 13:05:55 +0900

    Delete 자바 기본/자바 1일차 directory

--- 자바 기본/자바 1일차/Ex1.java
+++
@@ -1,8 +0,0 @@
-package am;
-
-public class Ex1 {
-	public static void main(String[] arags) { /* 메인 함수 args는 변수명이라 바꿔도 됨 그외는 건들지 X
-		프로그램의 시작 - 자바가상머신(JVM)이 자동으로 호출함*/
-		System.out.print("안녕하세요");
-	}
-}
