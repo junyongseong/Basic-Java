3575978155ff9629b65e83eeed1dc18d7767837a
성용준 <43407026+junyongseong@users.noreply.github.com>
2025-09-01 18:37:00 +0900

    Add files via upload

---
+++ Spring/ex0901_file/src/main/java/com/sist/ex0901_file/FileTestControl.java
@@ -0,0 +1,57 @@
+package com.sist.ex0901_file;
+
+import file.input.ParamVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+
+@Controller
+public class FileTestControl {
+    //파일 첨부에 필요한 멤버변수들
+    @Autowired
+    private ServletContext application;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    //파일이 저장될 위치 -webapp/resources/file_upload라는 폴더로 지정하자!
+    //위치 지정에 앞서 dispatcher-servlet.xml에서 webapp/resources를 등록해야함
+    private String upload_path = "/resources/file_upload";
+
+    @RequestMapping("/write")
+    public String write(){
+
+        return "write";
+    }
+    @RequestMapping(value = "/write", method = RequestMethod.POST)
+    public ModelAndView write(ParamVO pvo){
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/write");
+        MultipartFile f= pvo.getS_file();
+
+        String real_path = application.getRealPath(upload_path);
+        String fname= null;
+        if(f.getSize()>0){
+            fname = f.getOriginalFilename();
+            pvo.setFile_name(fname);
+//            pvo.setIp(request.getRemoteAddr()); ip받기
+
+            try{
+                f.transferTo(new File(real_path,fname));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+//        paramDAO.add(pvo);//DB저장
+
+        return mv;
+    }
+}
---
+++ Spring/ex0901_file/src/main/webapp/WEB-INF/view/write.jsp
@@ -0,0 +1,31 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+<h1> 파일 첨부 연습</h1>
+<form action="/write" method="post" enctype="multipart/form-data">
+  <label for="title">제목:</label>
+  <input type="text" id="title" name="title"/><br/>
+  <label for="content">내용:</label>
+  <textarea id="content" name="content"
+            rows="8" cols="50"></textarea><br/>
+
+  <label for="s_file">첨부 파일: </label>
+  <input type="file" id="s_file" name="s_file"/><br/>
+  <button type="button" onclick="exe(this.form)">
+    보내기
+  </button>
+</form>
+
+<script>
+  function exe(frm){
+    //유효성 검사 생략
+    frm.submit();
+  }
+</script>
+
+</body>
+</html>
